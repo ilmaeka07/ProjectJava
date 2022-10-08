9b99a5223d64db949f077b1c59d3bf842af1464f
ilmaeka07 <ilmaeka27@gmail.com>
2022-10-08 20:22:34 +0700

    add repo

---
+++ Bit.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectawal;
+
+/**
+ *
+ * @author Acer
+ */
+public class Bit {
+    public static void main(String[] args) {
+        int a=4, b=9, c;
+        c = a%b;
+        System.out.println("a AND b = " + c);
+        c = a|b;
+        System.out.println("a OR b = " + c);
+        c = a^b;
+        System.out.println("a EX OR b = " + c);
+        c = ~a;
+        System.out.println("NOT a = " + c);
+        c = a << 2;
+        System.out.println("a GESER KIRI = " + c);
+        c = a >> 2;
+        System.out.println("a GESER KANAN = " + c);
+    }
+}
---
+++ Percabangan.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectawal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class Percabangan {
+     public static void main(String[] args) {
+        Scanner masukan= new Scanner(System.in);
+        System.out.println("Masukan Angka = ");
+        int angka = masukan.nextInt();
+       if (angka%2==0) {
+           if (angka>10) {
+               System.out.println("Bilangan Genap dan");
+               System.out.println("Merupakan bilangan puluhan");
+           }
+       } else {
+           System.out.println("Angka " + angka + "adalah");
+           System.out.println("Bilangan Ganjil");
+       }
+       
+//       
+//        String hasil;
+//        hasil = angka%2==0 ? "Bilangan Genap":"Bilangan Ganjil";
+//        System.out.println(hasil);
+    }
+}
